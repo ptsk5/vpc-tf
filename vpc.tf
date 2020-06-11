c9c947a33e70d454d8b8b19f83377385ecc9a9f7
ptsk5 <47857057+ptsk5@users.noreply.github.com>
2020-06-12 01:00:02 +0200

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -88,6 +88,8 @@ resource ibm_is_floating_ip "fip1" {
   target = "${ibm_is_instance.vsi1.primary_network_interface.0.id}"
 }
 
+/*
 output sshcommand {
   value = "ssh root@${ibm_is_floating_ip.fip1.address}"
 }
+*/
